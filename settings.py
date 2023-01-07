8d85332452661028bdb43c3ad540a24d823fc4de
OYYOVEL <114139677+OYYOVEL@users.noreply.github.com>
2023-01-07 10:42:41 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 ### параметры для авторизации
 
-valid_email = "oyy@rambler.ru"
-valid_phone = "+79163642088"
+valid_email = "MAIL@MAIL.RU"
+valid_phone = "+79876543210"
 valid_pass = "z8s-7du-jum-SUZ"
